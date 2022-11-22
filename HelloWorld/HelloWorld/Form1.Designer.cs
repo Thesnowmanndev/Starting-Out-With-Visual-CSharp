7716dae36b7fbd8d10a82ecc1af2d7bbfb3a61c9
Kyle Martin <kyle.martin.0819@gmail.com>
2022-11-22 13:47:27 -0500

    Hello World Example
    - Initialized application
    - Added event handler to display a message box using the .NET framework

---
+++ HelloWorld/HelloWorld/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace HelloWorld
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.displayMessageButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // displayMessageButton
+            // 
+            this.displayMessageButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.displayMessageButton, "displayMessageButton");
+            this.displayMessageButton.Name = "displayMessageButton";
+            this.displayMessageButton.UseVisualStyleBackColor = true;
+            this.displayMessageButton.Click += new System.EventHandler(this.displayMessageButton_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.displayMessageButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button displayMessageButton;
+    }
+}
+
