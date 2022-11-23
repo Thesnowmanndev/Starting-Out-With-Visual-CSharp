4841aef7f2bdbb07d451cd503e5f5634c36d3487
Kyle Martin <kyle.martin.0819@gmail.com>
2022-11-23 16:50:34 -0500

    Completed tutorial 2-3
    - Created two labels: Directions and translated label
    - Created three buttons: Italian, Spanish, German
    - Created three on click events to print code to the translated label

---
+++ LanguageTranslatorApplication/LanguageTranslatorApplication/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace LanguageTranslatorApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.directionsLabel = new System.Windows.Forms.Label();
+            this.translationLabel = new System.Windows.Forms.Label();
+            this.italianButton = new System.Windows.Forms.Button();
+            this.spanishButton = new System.Windows.Forms.Button();
+            this.germanButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // directionsLabel
+            // 
+            this.directionsLabel.AutoSize = true;
+            this.directionsLabel.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.directionsLabel.Location = new System.Drawing.Point(27, 19);
+            this.directionsLabel.Name = "directionsLabel";
+            this.directionsLabel.Size = new System.Drawing.Size(416, 19);
+            this.directionsLabel.TabIndex = 0;
+            this.directionsLabel.Text = "Select a language and I will print \'Good Morning\' in that language.";
+            // 
+            // translationLabel
+            // 
+            this.translationLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.translationLabel.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.translationLabel.Location = new System.Drawing.Point(27, 60);
+            this.translationLabel.Name = "translationLabel";
+            this.translationLabel.Size = new System.Drawing.Size(416, 46);
+            this.translationLabel.TabIndex = 1;
+            this.translationLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // italianButton
+            // 
+            this.italianButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.italianButton.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.italianButton.Location = new System.Drawing.Point(27, 141);
+            this.italianButton.Name = "italianButton";
+            this.italianButton.Size = new System.Drawing.Size(90, 25);
+            this.italianButton.TabIndex = 2;
+            this.italianButton.Text = "Italian";
+            this.italianButton.UseVisualStyleBackColor = true;
+            this.italianButton.Click += new System.EventHandler(this.italianButton_Click);
+            // 
+            // spanishButton
+            // 
+            this.spanishButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.spanishButton.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.spanishButton.Location = new System.Drawing.Point(190, 141);
+            this.spanishButton.Name = "spanishButton";
+            this.spanishButton.Size = new System.Drawing.Size(90, 25);
+            this.spanishButton.TabIndex = 3;
+            this.spanishButton.Text = "Spanish";
+            this.spanishButton.UseVisualStyleBackColor = true;
+            this.spanishButton.Click += new System.EventHandler(this.spanishButton_Click);
+            // 
+            // germanButton
+            // 
+            this.germanButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.germanButton.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.germanButton.Location = new System.Drawing.Point(353, 141);
+            this.germanButton.Name = "germanButton";
+            this.germanButton.Size = new System.Drawing.Size(90, 25);
+            this.germanButton.TabIndex = 4;
+            this.germanButton.Text = "German";
+            this.germanButton.UseVisualStyleBackColor = true;
+            this.germanButton.Click += new System.EventHandler(this.germanButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 201);
+            this.Controls.Add(this.germanButton);
+            this.Controls.Add(this.spanishButton);
+            this.Controls.Add(this.italianButton);
+            this.Controls.Add(this.translationLabel);
+            this.Controls.Add(this.directionsLabel);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Language Translator Application";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label directionsLabel;
+        private Label translationLabel;
+        private Button italianButton;
+        private Button spanishButton;
+        private Button germanButton;
+    }
+}
---
+++ LanguageTranslatorApplication/README.md
@@ -0,0 +1,19 @@
+# Language Translation Application
+
+This is the source code folder of the Tutorial 2-3 application from pages 86-88 in chapter two. The point of this exercise is to learn how to edit a label control with code upon a click event.
+
+### Starting Application Preview:
+
+![Image of application upon start]()
+
+### Preview of user clicking the italian button:
+
+![Image of application upon click of the italian button]()
+
+### Preview of user clicking the spanish button:
+
+![Image of application upon click of the spanish button]()
+
+### Preview of user clicking the german button:
+
+![Image of application upon click of the german button]()
