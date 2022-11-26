198e706d8d156bdd95fcec3c0041de848b127129
Kyle Martin <kyle.martin.0819@gmail.com>
2022-11-26 12:45:15 -0500

    Card Flip Application
    - Added two pictureBox controls (faceDownCard, faceUpCard)
    - Added two button controls (showFaceDown, showFaceUp)
    - Added on-click events to flip card (change picturebox visibility to true or false)
    - Added preview images for github repo

---
+++ CardFlipApplication/CardFlipApplication/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CardFlipApplication
+{
+    public partial class CardFlipApplication : Form
+    {
+        public CardFlipApplication()
+        {
+            InitializeComponent();
+        }
+
+        private void faceDownButton_Click(object sender, EventArgs e)
+        {
+            faceUpCard.Visible = false;
+            faceDownCard.Visible = true;
+        }
+
+        private void faceUpButton_Click(object sender, EventArgs e)
+        {
+            faceDownCard.Visible = false;
+            faceUpCard.Visible = true;
+        }
+    }
+}
---
+++ CardFlipApplication/README.md
@@ -0,0 +1,11 @@
+# Card Flip Application
+
+The purpose of this application is to set the visibility state of a control when an on-click event occurs. When the application runs it will display a card face down (see Base Application image). When the user clicks the show face button the application with alter the visibility states of the picture box controls making it appear as if the card has flipped over.
+
+### Base Application:
+
+![image of application at runtime]()
+
+### Image of show face button clicked:
+
+![image of result after on-click event]()
